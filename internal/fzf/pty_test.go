a124ebb97f9647f674f954743dc91a50e4ae79bb
Samuel Lando <samuel.lando@aol.com>
2026-02-12 19:14:18 -0500

    Add some test cases

---
+++ internal/fzf/pty_test.go
@@ -0,0 +1,104 @@
+package fzf
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+)
+
+func TestStartPtyEcho(t *testing.T) {
+	stdinR, stdinW, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("stdin pipe: %v", err)
+	}
+	stdoutR, stdoutW, err := os.Pipe()
+	if err != nil {
+		stdinR.Close()
+		stdinW.Close()
+		t.Fatalf("stdout pipe: %v", err)
+	}
+	cmd := exec.Command("cat")
+	ptx, err := startPty(cmd, stdinR, stdoutW)
+	if err != nil {
+		stdinR.Close()
+		stdinW.Close()
+		stdoutR.Close()
+		stdoutW.Close()
+		t.Fatalf("startPty: %v", err)
+	}
+	t.Cleanup(func() {
+		ptx.Close()
+		stdinR.Close()
+		stdinW.Close()
+		stdoutR.Close()
+		stdoutW.Close()
+	})
+
+	payload := "hello from pty\n"
+	readCh := make(chan []byte, 1)
+	readErrCh := make(chan error, 1)
+	go func() {
+		data, err := io.ReadAll(ptx)
+		if err != nil {
+			readErrCh <- err
+			return
+		}
+		readCh <- data
+	}()
+
+	if _, err := ptx.Write([]byte(payload)); err != nil {
+		t.Fatalf("write: %v", err)
+	}
+	ptx.CloseInPipe()
+
+	waitCh := make(chan error, 1)
+	go func() {
+		waitCh <- cmd.Wait()
+	}()
+
+	select {
+	case err := <-waitCh:
+		if err != nil {
+			t.Fatalf("wait: %v", err)
+		}
+	case <-time.After(3 * time.Second):
+		t.Fatal("wait timeout")
+	}
+
+	select {
+	case err := <-readErrCh:
+		t.Fatalf("read: %v", err)
+	case data := <-readCh:
+		if string(data) != payload {
+			t.Fatalf("unexpected output: %q", string(data))
+		}
+	case <-time.After(3 * time.Second):
+		t.Fatal("read timeout")
+	}
+}
+
+func TestStartPtyMissingBinary(t *testing.T) {
+	stdinR, stdinW, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("stdin pipe: %v", err)
+	}
+	stdoutR, stdoutW, err := os.Pipe()
+	if err != nil {
+		stdinR.Close()
+		stdinW.Close()
+		t.Fatalf("stdout pipe: %v", err)
+	}
+	t.Cleanup(func() {
+		stdinR.Close()
+		stdinW.Close()
+		stdoutR.Close()
+		stdoutW.Close()
+	})
+
+	cmd := exec.Command("tmixer__missing_binary")
+	if _, err := startPty(cmd, stdinR, stdoutW); err == nil {
+		t.Fatal("expected error")
+	}
+}
