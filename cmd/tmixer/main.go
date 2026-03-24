846352c88da25949e6dbf1bf3910cfefc3064252
Samuel Lando <samuel.lando@icloud.com>
2026-03-24 12:48:41 -0400

    Fix broken tests, make sure to use the remaining args after perse flags

--- cmd/tmixer/main.go
+++ cmd/tmixer/main.go
@@ -42,7 +42,7 @@ func run(args ...string) (err error) {
 	ctx = logV2.ContextLogger(ctx, logV2.LogOptions{})
 	// Parse the arguments before setting up logging in case there's a extra log file
 	session.config = &config.Config{}
-	_, err = flags.ParseArgs(ctx, args, FLAGS, session.config)
+	args, err = flags.ParseArgs(ctx, args, FLAGS, session.config)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println()
