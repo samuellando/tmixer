e1178cdb581b43f8002de4fe4d42a31bddc8629f
Samuel Lando <samuel.lando@icloud.com>
2026-03-24 18:45:01 -0400

    Update refactor the flags package

---
+++ internal/flags/parse.go
@@ -0,0 +1,117 @@
+package flags
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"samuellando.com/tmixer/internal/config"
+	"samuellando.com/tmixer/internal/log/v2"
+)
+
+// Parse the args for the flags specified in inFlags
+// The following order of precedence is used.
+// 1. If a flags is present in args:
+//   - It will be set to the next arguments value
+//   - If it's a bare flag it's function will be called on an empty string
+//
+// 2. If the flag's environment variable is set, it will be set to that value
+// 3. FInally, if the flag has a default value, it will be set to that value
+func ParseArgs(ctx context.Context, args []string, inFlags map[string]Flag) (*config.Config, []string, error) {
+	conf := &config.Config{}
+	logEvent := log.Track(ctx, "flagParseEvent")
+	defer logEvent.Done()
+	logEvent.Log("inputArgs", args)
+
+	// Create a copy of the input, using pointers so we can keep track of what is set
+	flags := make(map[string]*Flag)
+	for name, flag := range inFlags {
+		cpFlag := flag
+		flags[name] = &cpFlag
+	}
+
+	remaining, err := setFlagsFromArgs(conf, args, flags)
+	if err != nil {
+		logEvent.Error(err)
+		return nil, nil, err
+	}
+	err = setFlagsFromEnvVars(conf, flags)
+	if err != nil {
+		logEvent.Error(err)
+		return nil, nil, err
+	}
+	err = setFlagsToDefault(conf, flags)
+	if err != nil {
+		logEvent.Error(err)
+		return nil, nil, err
+	}
+
+	logEvent.Log("remainingArgs", remaining)
+	logEvent.Log("result", *conf)
+	return conf, remaining, nil
+}
+
+func setFlagsFromArgs(conf *config.Config, args []string, flags map[string]*Flag) ([]string, error) {
+	remaining := make([]string, 0)
+	for i := 0; i < len(args); i++ {
+		matched := false
+		for name, flag := range flags {
+			if flag.matches(name, args[i]) {
+				matched = true
+				flag.isSet = true
+				var val string
+				if !flag.Bare {
+					if i+1 >= len(args) {
+						return nil, fmt.Errorf("Flag %s requires a value", name)
+					}
+					val = args[i+1]
+					i++
+				}
+				if parseErr := flag.ParseInput(val, conf); parseErr != nil {
+					err := fmt.Errorf("while parsing flag %s: %w", name, parseErr)
+					return nil, err
+				}
+				break
+			}
+		}
+		if !matched {
+			remaining = append(remaining, args[i])
+		}
+	}
+	return remaining, nil
+}
+
+func setFlagsFromEnvVars(conf *config.Config, flags map[string]*Flag) error {
+	for name, flag := range flags {
+		if !flag.isSet && flag.EnvironmentVariable != "" {
+			if val, set := os.LookupEnv(flag.EnvironmentVariable); set {
+				flag.isSet = true
+				if parseErr := flag.ParseInput(val, conf); parseErr != nil {
+					return fmt.Errorf("while parsing flag env var %s: %w", name, parseErr)
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func setFlagsToDefault(conf *config.Config, flags map[string]*Flag) error {
+	for name, flag := range flags {
+		if !flag.isSet && flag.Default != "" {
+			flag.isSet = true
+			if parseErr := flag.ParseInput(flag.Default, conf); parseErr != nil {
+				return fmt.Errorf("while parsing flag env var %s: %w", name, parseErr)
+			}
+		}
+	}
+	return nil
+}
+
+func isLongFlag(arg string) bool {
+	return strings.HasPrefix(arg, "--")
+}
+
+func isShortFlag(arg string) bool {
+	return strings.HasPrefix(arg, "-")
+}
--- internal/flags/parse_test.go
+++ internal/flags/parse_test.go
@@ -1,4 +1,4 @@
-package flags
+package flags_test
 
 import (
 	"context"
@@ -6,13 +6,15 @@ import (
 	"testing"
 
 	"samuellando.com/tmixer/internal/config"
+	"samuellando.com/tmixer/internal/flags"
 	"samuellando.com/tmixer/internal/log"
 )
 
 func TestParseArgs(t *testing.T) {
 	ctx, _ := log.New(context.Background(), nil)
 	counter := 0
-	flags := map[string]Flag{
+	args := []string{"--four", "--one", "aaa", "-t", "--three", "bbb", "hello"}
+	inFlags := map[string]flags.Flag{
 		"one": {
 			ShortName: "o",
 			ParseInput: func(s string, c *config.Config) error {
@@ -25,6 +27,7 @@ func TestParseArgs(t *testing.T) {
 		},
 		"two": {
 			ShortName: "t",
+			Bare:      true,
 			ParseInput: func(s string, c *config.Config) error {
 				if s != "" {
 					t.Fatal("incorrect input")
@@ -43,9 +46,9 @@ func TestParseArgs(t *testing.T) {
 			},
 		},
 		"four": {
-			Default: "ddd",
+			Bare: true,
 			ParseInput: func(s string, c *config.Config) error {
-				if s != "ddd" {
+				if s != "" {
 					t.Fatal("incorrect input")
 				}
 				counter += 1
@@ -54,6 +57,7 @@ func TestParseArgs(t *testing.T) {
 		},
 		"five": {
 			EnvironmentVariable: "TMIXER_TEST_FIVE",
+			Default:             "eee",
 			ParseInput: func(s string, c *config.Config) error {
 				if s != "env_var" {
 					t.Fatal("incorrect input")
@@ -62,55 +66,30 @@ func TestParseArgs(t *testing.T) {
 				return nil
 			},
 		},
+		"six": {
+			EnvironmentVariable: "TMIXER_TEST_SIX",
+			Default:             "fff",
+			ParseInput: func(s string, c *config.Config) error {
+				if s != "fff" {
+					t.Fatal("incorrect input")
+				}
+				counter += 1
+				return nil
+			},
+		},
 	}
-	args := []string{"--four", "--one", "aaa", "-t", "--three", "bbb", "hello"}
 	err := os.Setenv("TMIXER_TEST_FIVE", "env_var")
 	if err != nil {
 		t.Fatal(err)
 	}
-	remaining, err := ParseArgs(ctx, args, flags, nil)
+	_, remaining, err := flags.ParseArgs(ctx, args, inFlags)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if counter != 5 {
-		t.Fatal("Should've parsed 5 flags")
+	if counter != 6 {
+		t.Fatalf("Should've parsed 6 flags got %d", counter)
 	}
 	if len(remaining) != 1 || remaining[0] != "hello" {
 		t.Fatal("Should return remaining flags")
-	}
-}
-
-func TestHelpMessage(t *testing.T) {
-	flags := map[string]Flag{
-		"one": {
-			ShortName: "o",
-		},
-		"two": {
-			ShortName:   "t",
-			Description: "what it is",
-			Usage:       "How to use it",
-			Default:     "aaa",
-		},
-		"three": {},
-		"four": {
-			Default: "ddd",
-		},
-	}
-	res := HelpMessage(flags)
-	expected := `--four
-	Default: ddd
-
---one OR -o
-
---three
-
---two OR -t
-	what it is
-	Usage: How to use it
-	Default: aaa
-
-`
-	if res != expected {
-		t.Fatalf("%s\n!=\n%s", res, expected)
 	}
 }
