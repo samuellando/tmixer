cddf0118388815c6b5cd145cfbb4e23694d0dd13
Samuel Lando <samuel.lando@icloud.com>
2026-04-10 08:52:01 -0400

    refactor the client handshake logging

--- cmd/tmixer/client/client.go
+++ cmd/tmixer/client/client.go
@@ -23,13 +23,14 @@ import (
 
 var ErrNoSelection = errors.New("NO SELECTION MADE")
 
+// Run the client side application.
+// Connects to the tmixer server application, if it is not running it starts it,
+// If it is running on an outdated version it restarts it.
+// The client then performs a "handshake" with the server, by sending a command
+// and following the instructions.
+// If the command was start, and no other arguments were passed, a tmux client
+// is started.
 func Run(ctx context.Context, args ...string) (err error) {
-	err = options.FLAG_SET.Parse(args)
-	if err != nil {
-		return err
-	}
-	remaining := options.FLAG_SET.Args()
-	// Get a server session
 	client, conn, err := getServerConnection(ctx)
 	if err != nil {
 		return err
@@ -38,6 +39,12 @@ func Run(ctx context.Context, args ...string) (err error) {
 		err = errors.Join(err, conn.Close())
 	}()
 
+	err = options.FLAG_SET.Parse(args)
+	if err != nil {
+		return err
+	}
+	remaining := options.FLAG_SET.Args()
+	// Special commands
 	if len(remaining) >= 1 {
 		switch remaining[0] {
 		case "kill-server":
@@ -49,7 +56,7 @@ func Run(ctx context.Context, args ...string) (err error) {
 			return err
 		}
 	}
-
+	// Get a session and perform the handshake.
 	srv, err := client.Session(ctx)
 	if err != nil {
 		return err
@@ -59,59 +66,79 @@ func Run(ctx context.Context, args ...string) (err error) {
 	if err != nil {
 		return err
 	}
-	// If the bare start comman was used start a tmux client
+	// If the bare start command was used start a tmux client
 	if len(remaining) == 1 && remaining[0] == "start" {
 		return errors.Join(err, startClient(selected))
 	}
 	return nil
 }
 
+// Client server handshake.
+// 1. the client send the input arguments to the server
+// 2. the server responds:
+//   - with EOF, meaning the session is done
+//   - an error
+//   - a list of project names, meaning a selection must be returned
+//   - and/or an output message, which should be printed
+//
+// 3. The communication must keep going until the server returns EOF.
+// 4. If no selection can be made the client may exit.
 func handshake(ctx context.Context, srv grpc.BidiStreamingClient[protocol.Request, protocol.Response], args []string) (string, error) {
 	config, err := options.Config(ctx)
 	if err != nil {
 		return "", err
 	}
 	logEvent := log.Track(ctx, "serverCommunication")
-	requests := make([]string, 0)
-	responses := make([]string, 0)
+	type exchange struct {
+		Request  string
+		Response string
+		Time     time.Time
+		Duration time.Duration
+	}
+	exchanges := make([]exchange, 0)
 	defer func() {
-		logEvent.Log("requests", requests)
-		logEvent.Log("responses", responses)
+		logEvent.Log("exchanges", exchanges)
 	}()
 	defer logEvent.Done()
 
 	var selected string
-
+	// Initial request is always the args.
 	req := &protocol.Request{Payload: &protocol.Request_Args{Args: &protocol.Args{Args: args}}}
-	requests = append(requests, req.String())
+	exc := exchange{Time: time.Now(), Request: req.String()}
 	err = srv.Send(req)
 	if err != nil {
 		return "", err
 	}
 	for {
 		resp, err := srv.Recv()
-		responses = append(responses, resp.String())
+		exc.Response = resp.String()
+		exc.Duration = time.Since(exc.Time)
+		exchanges = append(exchanges, exc)
 		if err != nil {
 			// When the server hangs up, we can exit.
 			if err == io.EOF {
 				break
 			}
+			logEvent.Error(err)
 			return "", err
 		}
 		switch resp.Payload.(type) {
 		case *protocol.Response_NeedsSelection:
 			selection, err := fzf.Pick(ctx, config, resp.GetNeedsSelection().Projects)
 			if err != nil {
+				logEvent.Error(err)
 				return "", err
 			}
 			if selection == nil {
 				return "", ErrNoSelection
 			}
-
-			req := &protocol.Request{Payload: &protocol.Request_Selection{Selection: &protocol.Selection{Project: selection}}}
-			requests = append(requests, req.String())
+			req := &protocol.Request{Payload: &protocol.Request_Selection{
+				Selection: &protocol.Selection{Project: selection}},
+			}
+			exc = exchange{Time: time.Now(), Request: req.String()}
 			err = srv.Send(req)
 			if err != nil {
+				logEvent.Error(err)
 				return "", err
 			}
 		case *protocol.Response_Output:
@@ -123,13 +150,16 @@ func handshake(ctx context.Context, srv grpc.BidiStreamingClient[protocol.Reques
 				selected = *resp.Selected
 			}
 		case *protocol.Response_Err:
-			return "", errors.New(*resp.GetErr().Error)
+			err := errors.New(*resp.GetErr().Error)
+			logEvent.Error(err)
+			return "", err
 		}
 	}
 	logEvent.Log("selection", selected)
 	return selected, nil
 }
 
+// Start an embedded tmux client in the terminal, if not already in one.
 func startClient(proj string) error {
 	if _, is_set := os.LookupEnv("TMUX"); is_set {
 		return nil
@@ -145,6 +175,9 @@ func startClient(proj string) error {
 	return cmd.Wait()
 }
 
+// Connect to the tmixer server.
+// If not running, start it as a background process.
+// If it's running, but on a different version, restart it.
 func getServerConnection(ctx context.Context) (protocol.TmixerClient, *grpc.ClientConn, error) {
 	logEvent := log.Track(ctx, "getServerConnection")
 	defer logEvent.Done()
@@ -193,6 +226,9 @@ func getServerConnection(ctx context.Context) (protocol.TmixerClient, *grpc.Clie
 				break
 			}
 			time.Sleep(20 * time.Millisecond)
+		}
+		if err != nil {
+			return nil, nil, fmt.Errorf("start updated server: %v", err)
 		}
 		return getServerConnection(ctx)
 	}
